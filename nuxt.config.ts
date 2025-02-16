ff5ae2b7a60f16e3dd761e00c0298689e5a4eea1
kingofdreams777 <rossmartin@sandiego.edu>
2025-02-15 18:53:25 -0800

    starting code for epk website update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    modules: [
-        "@nuxt/image",
-        "nuxt-icon",
-        "@nuxt/content",
-        "@nuxtjs/tailwindcss",
-        "@pinia/nuxt"
-    ],
+  devtools: { enabled: true },
+
+  modules: [
+      "@nuxt/image",
+      "nuxt-icon",
+      "@nuxt/content",
+      "@nuxtjs/tailwindcss",
+      "@pinia/nuxt"
+  ],
+
+  compatibilityDate: "2025-02-15",
 })
---
+++ pages/epk.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div class="hero min-h-fit">
+      <div class="hero-content">
+        <div class="prose sm:prose-xl text-center">
+          <h1>Electronic Press Kit</h1>
+        </div>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="prose flex flex-col m-auto text-center">
+      <h1>Biography</h1>
+      <p>
+        Alternate Era is an alternative hip-hop duo blending ethereal production, cinematic soundscapes, and high-energy
+        lyricism. Inspired by Gorillaz, they fuse hip-hop, electronic, and experimental rock to create immersive,
+        genre-defying music. With a bi-coastal creative process between San Diego (Ross –
+        producer/multi-instrumentalist) and Manhattan (Chris – lyricist/rapper), their sound is both futuristic and
+        nostalgic. Whether through hypnotic beats, introspective lyrics, or electrifying performances, Alternate Era
+        delivers a sonic experience that pushes boundaries.
+      </p>
+
+    </div>
+  </div>
+</template>
