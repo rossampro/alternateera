9eecf4c110125741c41c16194d3dec71e6ce824f
kingofdreams777 <rossmartin@sandiego.edu>
2025-02-15 19:43:01 -0800

    updating seo and head:

--- app.vue
+++ app.vue
@@ -1,8 +1,25 @@
+<script setup lang="ts">
+useHead({
+  title: "Alternate Era",
+  meta: [
+    {
+      name: "description",
+      content: "Official website for Alternate Era"
+    }
+  ]
+})
+
+useSeoMeta({
+  title: "Alternate Era",
+  description: "Official website for Alternate Era"
+
+})
+</script>
 <template>
-    <div>
-        <NuxtLayout>
-            <LazyNuxtLoadingIndicator />
-            <LazyNuxtPage />
-        </NuxtLayout>
-    </div>
+  <div>
+    <NuxtLayout>
+      <LazyNuxtLoadingIndicator />
+      <LazyNuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
