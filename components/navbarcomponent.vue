4b6667397eb02dcbc4a4addaf9132e6308a1625d
Ross AM <rossmartin@sandiego.edu>
2025-03-07 13:59:02 -0800

    music page update

--- components/navbarcomponent.vue
+++ components/navbarcomponent.vue
@@ -8,14 +8,13 @@
                     <Icon name="heroicons:home-solid" class="w-8 h-8"></Icon>
                 </LazyNuxtLink>
             </label>
-            <!--
-            TODO: Get these pages finished
 
             <label class="btn btn-ghost btn-circle">
                 <LazyNuxtLink to="/music">
                     <Icon name="heroicons:musical-note-solid" class="w-8 h-8"></Icon>
                 </LazyNuxtLink>
             </label>
+            <!--
             <label class="btn btn-ghost btn-circle">
                 <LazyNuxtLink to="/videos">
                     <Icon name="heroicons:video-camera-solid" class="w-8 h-8"></Icon>
@@ -27,6 +26,8 @@
             <LazyNuxtLink to="/" class="btn btn-ghost">
                 <NuxtPicture width="100" height="50" format="webp" src="/alteralogotransparent.png" />
             </LazyNuxtLink>
+        </div>
+        <div class="navbar-end">
         </div>
     </div>
 </template>
--- pages/music.vue
+++ pages/music.vue
@@ -3,13 +3,7 @@
         <div class="prose sm:prose-xl text-center">
             <h1>Music</h1>
             <div class="hero-content">
-                <Spotifyembed></Spotifyembed>
-                <a href="https://music.apple.com/us/artist/alternate-era/1387195090?itscg=30200&itsct=music_box_badge&ls=1&app=music&mttnsubad=1387195090"
-                    style="display: inline-block;">
-                    <img src="https://toolbox.marketingtools.apple.com/api/v2/badges/listen-on-apple-music/badge/en-us"
-                        alt="Listen on Apple Music"
-                        style="width: 282px; height: 82px; vertical-align: middle; object-fit: contain;" />
-                </a>
+                <LazyMusiccomponent></LazyMusiccomponent>
             </div>
         </div>
     </div>
