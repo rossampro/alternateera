1ef17bef7dd6f5527e7ed04e0fd22a9f7734f73a
Ross AM <rossmartin@sandiego.edu>
2025-03-20 08:20:54 -0700

    adding a featured song component that can be easily updated to portray our latest song

---
+++ components/featuredsongcomponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="prose text-center underline">
+        <h1>Featured Song</h1>
+        <div class="mockup-phone border border-base-300 bg-base-300">
+            <div class="mockup-phone-camera">
+            </div>
+            <div class="mockup-phone-display m-fit h-fit">
+                <iframe width="464" height="825" src="https://www.youtube.com/embed/JNdUTsWolgE"
+                    title="Sway With Me (ft Ross A.M.) by Alternate Era" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</template>
