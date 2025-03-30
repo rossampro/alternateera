050a5c922c3ee39399807698ef9963e521f74e6f
Ross A.M. <rossmartin@sandiego.edu>
2025-03-30 15:30:19 -0700

    contact form redesigned

--- components/contactform.vue
+++ components/contactform.vue
@@ -1,155 +1,74 @@
 <template>
-    <div class="card bg-base-300">
-        <div id="mc_embed_shell">
-            <div id="mc_embed_signup">
-                <form
-                    action="https://gmail.us20.list-manage.com/subscribe/post?u=9848c570f48b92b911e6a7195&amp;id=0578cfc38c&amp;f_id=00b759eef0"
-                    method="post"
-                    id="mc-embedded-subscribe-form"
-                    name="mc-embedded-subscribe-form"
-                    class="validate"
-                    target="_self"
-                    novalidate="true"
-                >
-                    <div id="mc_embed_signup_scroll">
-                        <h2>Subscribe</h2>
-                        <div class="indicates-required">
-                            <span class="asterisk">*</span> indicates required
-                        </div>
-                        <div class="mc-field-group">
-                            <label for="mce-EMAIL"
-                                >Email Address
-                                <span class="asterisk">*</span></label
-                            ><input
-                                type="email"
-                                name="EMAIL"
-                                class="required email"
-                                id="mce-EMAIL"
-                                required="true"
-                                value=""
-                            />
-                        </div>
-                        <div class="mc-field-group">
-                            <label for="mce-FNAME">First Name </label
-                            ><input
-                                type="text"
-                                name="FNAME"
-                                class="text"
-                                id="mce-FNAME"
-                                value=""
-                            />
-                        </div>
-                        <div
-                            id="mergeRow-gdpr"
-                            class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group"
-                        >
-                            <div class="content__gdpr">
-                                <label>Marketing Permissions</label>
-                                <p>
-                                    Please select all the ways you would like to
-                                    hear from Alternate Era:
-                                </p>
-                                <fieldset
-                                    class="mc_fieldset gdprRequired mc-field-group"
-                                    name="interestgroup_field"
-                                >
-                                    <label class="subfield" for="gdpr38151"
-                                        ><input
-                                            type="checkbox"
-                                            id="gdpr_38151"
-                                            name="gdpr[38151]"
-                                            class="gdpr checkbox"
-                                            value="Y"
-                                        /><span>Email</span></label
-                                    >
-                                </fieldset>
-                                <p>
-                                    You can unsubscribe at any time by clicking
-                                    the link in the footer of our emails. For
-                                    information about our privacy practices,
-                                    please visit our website.
-                                </p>
-                            </div>
-                            <div class="content__gdprLegal">
-                                <p>
-                                    We use Mailchimp as our marketing platform.
-                                    By clicking below to subscribe, you
-                                    acknowledge that your information will be
-                                    transferred to Mailchimp for processing.
-                                    <a href="https://mailchimp.com/legal/terms"
-                                        >Learn more</a
-                                    >
-                                    about Mailchimp's privacy practices.
-                                </p>
-                            </div>
-                        </div>
-                        <div hidden="">
-                            <input type="hidden" name="tags" value="4275206" />
-                        </div>
-                        <div id="mce-responses" class="clear foot">
-                            <div
-                                class="response"
-                                id="mce-error-response"
-                                style="display: none"
-                            ></div>
-                            <div
-                                class="response"
-                                id="mce-success-response"
-                                style="display: none"
-                            ></div>
-                        </div>
-                        <div
-                            aria-hidden="true"
-                            style="position: absolute; left: -5000px"
-                        >
-                            /* real people should not fill this in and expect
-                            good things - do not remove this or risk form bot
-                            signups */
-                            <input
-                                type="text"
-                                name="b_9848c570f48b92b911e6a7195_0578cfc38c"
-                                tabindex="-1"
-                                value=""
-                            />
-                        </div>
-                        <div class="optionalParent">
-                            <div class="clear foot">
-                                <input
-                                    type="submit"
-                                    name="subscribe"
-                                    id="mc-embedded-subscribe"
-                                    class="button btn btn-primary"
-                                    value="Subscribe"
-                                />
-                                <p style="margin: 0px auto">
-                                    <a
-                                        href="http://eepurl.com/i8fD3M"
-                                        title="Mailchimp - email marketing made easy and fun"
-                                        ><span
-                                            style="
-                                                display: inline-block;
-                                                background-color: transparent;
-                                                border-radius: 4px;
-                                            "
-                                            ><img
-                                                class="refferal_badge"
-                                                src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
-                                                alt="Intuit Mailchimp"
-                                                style="
-                                                    width: 220px;
-                                                    height: 40px;
-                                                    display: flex;
-                                                    padding: 2px 0px;
-                                                    justify-content: center;
-                                                    align-items: center;
-                                                " /></span
-                                    ></a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+    <div class="bg-gradient-to-r from-primary via-neutral to-primary p-8 rounded-lg shadow-lg max-w-lg mx-auto">
+        <form
+            action="https://gmail.us20.list-manage.com/subscribe/post?u=9848c570f48b92b911e6a7195&amp;id=0578cfc38c&amp;f_id=00b759eef0"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            class="space-y-6"
+            target="_self"
+            novalidate="true"
+        >
+            <h2 class="text-3xl font-bold text-center text-base-content">Subscribe</h2>
+            <div class="text-center text-sm text-base-content">
+                <span class="text-accent">*</span> indicates required
             </div>
-        </div>
+            <div class="space-y-4">
+                <div class="flex flex-col">
+                    <label for="mce-EMAIL" class="text-base-content font-semibold">Email Address <span class="text-accent">*</span></label>
+                    <input
+                        type="email"
+                        name="EMAIL"
+                        class="input input-bordered w-full"
+                        id="mce-EMAIL"
+                        required
+                        value=""
+                    />
+                </div>
+                <div class="flex flex-col">
+                    <label for="mce-FNAME" class="text-base-content font-semibold">First Name</label>
+                    <input
+                        type="text"
+                        name="FNAME"
+                        class="input input-bordered w-full"
+                        id="mce-FNAME"
+                        value=""
+                    />
+                </div>
+            </div>
+            <div class="bg-base-200 p-4 rounded-lg">
+                <label class="text-base-content font-semibold">Marketing Permissions</label>
+                <p class="text-sm text-base-content">
+                    Please select all the ways you would like to hear from Alternate Era:
+                </p>
+                <fieldset class="space-y-2">
+                    <label class="flex items-center space-x-2">
+                        <input
+                            type="checkbox"
+                            id="gdpr_38151"
+                            name="gdpr[38151]"
+                            class="checkbox"
+                            value="Y"
+                        />
+                        <span class="text-base-content">Email</span>
+                    </label>
+                </fieldset>
+                <p class="text-xs text-base-content mt-2">
+                    You can unsubscribe at any time by clicking the link in the footer of our emails. For information about our privacy practices, please visit our website.
+                </p>
+            </div>
+            <div class="text-xs text-base-content">
+                We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing. <a href="https://mailchimp.com/legal/terms" class="text-accent underline">Learn more</a> about Mailchimp's privacy practices.
+            </div>
+            <div class="flex justify-center">
+                <input
+                    type="submit"
+                    name="subscribe"
+                    id="mc-embedded-subscribe"
+                    class="btn btn-primary w-full"
+                    value="Subscribe"
+                />
+            </div>
+        </form>
     </div>
 </template>
