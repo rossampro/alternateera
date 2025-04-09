259a33258354df3626815f2629af1d9366e619d4
Ross A.M. <64719635+rossampro@users.noreply.github.com>
2025-04-08 17:55:41 -0700

    update links to include our patreon (#18)

--- composables/links.ts
+++ composables/links.ts
@@ -42,6 +42,11 @@ const links: Link[] = [
         "https://bsky.app/profile/alternateera.bsky.social",
         "simple-icons:bluesky",
     ),
+    new Link(
+        "Patreon",
+        "https://patreon.com/AlternateEra?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink",
+        "simple-icons:patreon",
+    ),
 ];
 
 export function GetSocialLinks() {
