5db27fc3bf676c7acac6edde5874f1bba404a4d8
Ross A.M. <64719635+rossampro@users.noreply.github.com>
2025-04-11 09:57:49 -0700

    fix social media links to appear correctly on mobile (#20)

--- components/socialmediacomponent.vue
+++ components/socialmediacomponent.vue
@@ -2,17 +2,12 @@
 const links = GetSocialLinks();
 </script>
 <template>
-    <div class="prose hero-content m-auto flex flex-col text-center">
+    <div class="prose hero-content m-auto flex md:flex-col flex-row text-center">
         <div class="flex justify-center space-x-4">
             <div v-for="link in links" :key="link.uri">
-                <NuxtLink
-                    :to="link.uri"
-                    class="btn btn-circle join-item from-primary via-secondary to-accent transform transition-transform hover:scale-110 hover:bg-gradient-to-r"
-                >
-                    <Icon
-                        :name="link.icon"
-                        class="text-base-content text-2xl"
-                    ></Icon>
+                <NuxtLink :to="link.uri"
+                    class="btn btn-circle join-item from-primary via-secondary to-accent transform transition-transform hover:scale-110 hover:bg-gradient-to-r">
+                    <Icon :name="link.icon" class="text-base-content text-2xl"></Icon>
                 </NuxtLink>
             </div>
         </div>
