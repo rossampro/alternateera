78b44e5451f09a7e6de2f2d5a19d2e33ac777171
Ross AM <rossmartin@sandiego.edu>
2025-04-24 15:57:16 -0700

    update human xperiment song description, load youtube videos on view

--- components/videoplayer.vue
+++ components/videoplayer.vue
@@ -1,27 +1,15 @@
 <template>
-    <ScriptYouTubePlayer :video-id="videoId" class="h-96 w-full md:h-[32rem]">
+    <ScriptYouTubePlayer :video-id="videoId" class="h-96 w-full md:h-[32rem]" trigger="visible">
         <template #placeholder>
             <div class="relative h-full w-full">
-                <img
-                    :src="`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`"
-                    alt="Video Thumbnail"
-                    class="h-full w-full object-cover rounded-lg"
-                />
+                <img :src="`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`" alt="Video Thumbnail"
+                    class="h-full w-full object-cover rounded-lg" />
                 <div class="absolute inset-0 flex items-center justify-center">
                     <div class="bg-black bg-opacity-50 p-3 rounded-full">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="h-12 w-12 text-white"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M14.752 11.168l-4.586-2.664A1 1 0 009 9.402v5.196a1 1 0 001.166.986l4.586-2.664a1 1 0 000-1.736z"
-                            />
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M14.752 11.168l-4.586-2.664A1 1 0 009 9.402v5.196a1 1 0 001.166.986l4.586-2.664a1 1 0 000-1.736z" />
                         </svg>
                     </div>
                 </div>
@@ -31,8 +19,6 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
-
 defineProps({
     videoId: {
         type: String,
--- pages/featuredartists.vue
+++ pages/featuredartists.vue
@@ -18,57 +18,29 @@ const featuredArtists = [
             {
                 id: "KF__fi6r6Ko",
                 title: "Look in the Mirror",
-                description: "Song 'Look in the Mirror' off the album 'Method Development' was written as a reminder to always embrace your inner child no matter how old you become.  We should never forget to play and enjoy life and not be diluted by social norms.",
+                description: "Song 'Look in the Mirror' off the album 'Method Development' is a song about self reflection and how as individuals we are just as capable of deceiving ourselves as we are at being deceived by outside forces.",
             },
         ],
     },
-    // {
-    //     name: "Artist Two",
-    //     bio: "Artist Two combines hip-hop and electronic beats to create a unique sound.",
-    //     photo: "/public/artist-two.jpg",
-    //     videos: [
-    //         {
-    //             id: "video4",
-    //             title: "Track Four",
-    //             description: "A powerful anthem by Artist Two.",
-    //         },
-    //         {
-    //             id: "video5",
-    //             title: "Track Five",
-    //             description: "A track that showcases their versatility.",
-    //         },
-    //         {
-    //             id: "video6",
-    //             title: "Track Six",
-    //             description: "An experimental journey.",
-    //         },
-    //     ],
-    // },
 ];
 </script>
 
 <template>
     <div>
-      <div class="mb-10 flex flex-col items-center text-center">
+        <div class="mb-10 flex flex-col items-center text-center">
             <h1 class="text-4xl font-bold max-w-2xl mt-10">Featured Artists</h1>
-          <div class="card-body mt-4 bg-base-200 shadow">
-            <p class="mt-4 text-lg text-base-content max-w-xl">
-            Alternate Era is more than just a band — it's a movement redefining music and creativity. 
-            Here, we connect enlightened individuals with earthshaking artists who challenge conventions 
-            and inspire through their groundbreaking work. Explore and join the journey of artistic evolution.
-            </p>
-          </div>
-      </div>
+            <div class="card-body mt-4 bg-base-200 shadow">
+                <p class="mt-4 text-lg text-base-content max-w-xl">
+                    Alternate Era is more than just a band — it's a movement redefining music and creativity.
+                    Here, we connect enlightened individuals with earthshaking artists who challenge conventions
+                    and inspire through their groundbreaking work. Explore and join the journey of artistic evolution.
+                </p>
+            </div>
+        </div>
         <div v-for="artist in featuredArtists" :key="artist.name" class="mb-10">
             <!-- Hero Section -->
-            <div
-                class="bg-base-200 flex flex-col items-center rounded-lg p-6 shadow-lg md:flex-row"
-            >
-                <LazyNuxtImg
-                    :src="artist.photo"
-                    :alt="artist.name"
-                    class="w-full rounded-lg shadow-md md:w-1/3"
-                />
+            <div class="bg-base-200 flex flex-col items-center rounded-lg p-6 shadow-lg md:flex-row">
+                <LazyNuxtImg :src="artist.photo" :alt="artist.name" class="w-full rounded-lg shadow-md md:w-1/3" />
                 <div class="mt-4 md:mt-0 md:ml-6">
                     <h2 class="text-3xl font-bold">{{ artist.name }}</h2>
                     <p class="text-base-content mt-2">{{ artist.bio }}</p>
@@ -76,18 +48,11 @@ const featuredArtists = [
             </div>
 
             <!-- Videos Section -->
-            <div
-                class="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
-            >
-                <div
-                    v-for="video in artist.videos"
-                    :key="video.id"
-                    class="music-card bg-base-200 rounded-lg p-5 shadow-lg transition-shadow duration-300 hover:shadow-xl"
-                >
-                    <videoplayer
-                        :video-id="video.id"
-                        class="animate-fade-in shadow-eg h-64 w-full rounded-lg lg:h-96"
-                    />
+            <div class="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                <div v-for="video in artist.videos" :key="video.id"
+                    class="music-card bg-base-200 rounded-lg p-5 shadow-lg transition-shadow duration-300 hover:shadow-xl">
+                    <videoplayer :video-id="video.id"
+                        class="animate-fade-in shadow-eg h-64 w-full rounded-lg lg:h-96" />
                     <h3 class="mb-2 text-lg font-semibold sm:text-xl">
                         {{ video.title }}
                     </h3>
@@ -100,13 +65,13 @@ const featuredArtists = [
         <div class="mt-10 text-center">
             <h2 class="text-2xl font-bold">Join the Movement</h2>
             <div class="card-body mt-4 bg-base-200 shadow">
-              <p class="mt-4 text-lg text-base-content max-w-xl mx-auto">
-            Are you an artist or a fan who wants to lead the charge in the music revolution? 
-            Sign up for our email list and stay connected with the latest updates, events, and opportunities.
-              </p>
+                <p class="mt-4 text-lg text-base-content max-w-xl mx-auto">
+                    Are you an artist or a fan who wants to lead the charge in the music revolution?
+                    Sign up for our email list and stay connected with the latest updates, events, and opportunities.
+                </p>
             </div>
             <div class="mt-6">
-          <contactform />
+                <contactform />
             </div>
         </div>
     </div>
