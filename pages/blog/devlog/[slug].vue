7b1c3e0fffac85d25a2076ff0629c4efaadc07ec
Remus Alexander <remusalexander@protonmail.com>
2025-05-11 11:21:07 -0700

    blog addition coming along

---
+++ pages/blog/devlog/[slug].vue
@@ -0,0 +1,13 @@
+<script setup>
+const slug = useRoute().params.slug
+const { data: post } = await useAsyncData(`devlog-${slug}`, () => {
+  return queryCollection('content').path(`/devlog/${slug}`).first()
+})
+</script>
+
+<template>
+  <!-- Render the blog post as Prose & Vue components -->
+  <div class="prose prose-lg mx-auto mt-8">
+    <ContentRenderer :value="post" />
+  </div>
+</template>
