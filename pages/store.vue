63f23516531281d09a56f407199c651683556791
Ross A.M. <64719635+rossampro@users.noreply.github.com>
2025-06-16 18:23:46 -0700

    store page update (#27)

---
+++ pages/store.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="h-screen w-screen">
+        <iframe
+            src="https://alternate-era.printify.me/"
+            frameborder="0"
+            class="h-full w-full"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            sandbox="allow-forms allow-scripts allow-same-origin"
+        ></iframe>
+    </div>
+</template>
