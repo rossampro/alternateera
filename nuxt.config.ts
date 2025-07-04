886dde5ba87b41c4b51fa51f6426945f3475b59f
Ross A.M. <64719635+rossampro@users.noreply.github.com>
2025-07-04 09:07:22 -0700

    add google analytics (#28)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,55 +3,62 @@ import { defineOrganization } from "nuxt-schema-org/schema";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    app: {
-        head: {
-            script: [
-                {
-                    async: true,
-                    src: '/js/mailerlitepopup.js',
-                }
-            ]
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        {
+          async: true,
+          src: '/js/mailerlitepopup.js',
         }
-    },
-    css: ["~/assets/app.css"],
-    site: {
-        name: 'Alternate Era'
-    },
-    schemaOrg: {
-        identity: defineOrganization({
-            name: "Alternate Era",
-            logo: "/alteralogotransparent.png"
-        })
-    },
-    seo: {
-        meta: {
-            title: "Alternate Era",
-            description: "Mind-bending Alternative Hip Hop Fusion",
-            ogImage: "/og-image.png",
-            ogTitle: "Alternate Era",
-            ogDescription: "Mind-bending Alternative Hip Hop Fusion"
-        }
-    },
-    vite: {
-        plugins: [tailwindcss()],
-    },
+      ]
+    }
+  },
+  css: ["~/assets/app.css"],
+  site: {
+    name: 'Alternate Era'
+  },
+  schemaOrg: {
+    identity: defineOrganization({
+      name: "Alternate Era",
+      logo: "/alteralogotransparent.png"
+    })
+  },
+  seo: {
+    meta: {
+      title: "Alternate Era",
+      description: "Mind-bending Alternative Hip Hop Fusion",
+      ogImage: "/og-image.png",
+      ogTitle: "Alternate Era",
+      ogDescription: "Mind-bending Alternative Hip Hop Fusion"
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  scripts: {
+    registry: {
+      googleAnalytics: {
+        id: 'G-DZS2CF48HP'
+      }
+    }
+  },
 
-    modules: [
-      "@nuxt/image",
-      "@nuxt/icon",
-      "nuxt-meta-pixel",
-      "@nuxtjs/seo",
-      "@nuxt/scripts",
-      "@nuxt/content",
-    ],
+  modules: [
+    "@nuxt/image",
+    "@nuxt/icon",
+    "nuxt-meta-pixel",
+    "@nuxtjs/seo",
+    "@nuxt/scripts",
+    "@nuxt/content",
+  ],
 
-    compatibilityDate: "2025-02-15",
-    runtimeConfig: {
-        public: {
-            metapixel: {
-                default: { id: "" },
-            },
-        },
+  compatibilityDate: "2025-02-15",
+  runtimeConfig: {
+    public: {
+      metapixel: {
+        default: { id: "" },
+      },
     },
+  },
 });
